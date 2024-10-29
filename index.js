b745de39121eddb31fc44f292d4e28339979be77
bs <bartosz.staniak@gmail.com>
2024-10-29 19:50:49 +0100

    incorrectly attempted to print out the first element of the array

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 {
     const array1 = [1, 2, 3, 4]; // using the 'const' keyword limits the scope in which constant is visible
-    console.log();
+    console.log(array1.0);
     console.log(array1[0]);
 }
 

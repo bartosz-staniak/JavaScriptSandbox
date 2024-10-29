30fda6fd7259031e310dd530b535da603b9efa47
bs <bartosz.staniak@gmail.com>
2024-10-29 20:15:20 +0100

    copied the previous line of code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
     const array1 = [1, 2, 3, 4]; // using the 'const' keyword limits the scope in which constant is visible
     // console.log(array1.0);
     console.log(array1.$0); console.log(typeof array1.$0);
-    
+    console.log(array1.$0); console.log(typeof array1.$0);
     console.log(array1[0]);
 }
 

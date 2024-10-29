2bd02d7ef232449524833f18f516a35ea806e1c9
bs <bartosz.staniak@gmail.com>
2024-10-29 19:45:38 +0100

    accessed the first element in the array

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 {
     const array1 = [1, 2, 3, 4]; // using the 'const' keyword limits the scope in which constant is visible
-    console.log(array1[]);
+    console.log(array1[0]);
 }
 
 // console.log(array1); // attempt to use a constant outside of its scope causes a crash as it is not defined

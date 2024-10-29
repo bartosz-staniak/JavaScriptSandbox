34f4cd7a51e96b6f81580cb2e5bd7f965253460d
bs <bartosz.staniak@gmail.com>
2024-10-29 16:50:02 +0100

    commented out the line of code which causes the app to crash

--- index.js
+++ index.js
@@ -4,4 +4,4 @@
     const array1 = [1, 2, 3, 4]; // using the 'const' keyword limits the scope in which constant is visible
 }
 
-console.log(array1); // attempt to use a constant outside of its scope causes a crash as it is not defined
+// console.log(array1); // attempt to use a constant outside of its scope causes a crash as it is not defined

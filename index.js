850cc209b1ae0ba31e552096d9398a95643cdfda
bs <bartosz.staniak@gmail.com>
2024-10-30 20:11:58 +0100

    commented out the constant containing array of numbers as value

--- index.js
+++ index.js
@@ -157,7 +157,7 @@
 }
 
 {
-    const array1 = [1, 2, 3, 4];
+    // const array1 = [1, 2, 3, 4];
 
     const initialValue = 1; // a substitute for the 'previous result' the first time a callback function is run?
     let cycleNo = 1;

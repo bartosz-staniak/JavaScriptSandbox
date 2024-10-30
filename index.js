8e5f8220b175ab91fc6cd9decbc7755ff22aaef1
bs <bartosz.staniak@gmail.com>
2024-10-30 16:02:43 +0100

    added the 'return' keyword which bugs the function because the result is '1' instead of '10'

--- index.js
+++ index.js
@@ -91,7 +91,7 @@
     const initialValue = 1; // a substitute for the 'previous result' the first time a callback function is run?
     const sumWithInitial = array1.reduce(
         (sum, iteratedValue) => {
-            sum + iteratedValue, initialValue
+            return sum + iteratedValue, initialValue
         }
     );
 

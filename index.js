df381930b5127a0eb0d5838e2810a26e83f5a96e
bs <bartosz.staniak@gmail.com>
2024-10-30 16:01:32 +0100

    copied the last block of code enclosed in statement braces '{}'

--- index.js
+++ index.js
@@ -84,3 +84,16 @@
 
     console.log("Initial value: " + initialValue + ", Sum of the values in the array: " + sumWithInitial);
 }
+
+{
+    const array1 = [1, 2, 3, 4];
+
+    const initialValue = 1; // a substitute for the 'previous result' the first time a callback function is run?
+    const sumWithInitial = array1.reduce(
+        (sum, iteratedValue) => {
+            sum + iteratedValue, initialValue
+        }
+    );
+
+    console.log("Initial value: " + initialValue + ", Sum of the values in the array: " + sumWithInitial);
+}

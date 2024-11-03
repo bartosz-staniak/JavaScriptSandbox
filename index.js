dd6842dd8eb9c34fd01e32fdebe59559bf46b203
bs <bartosz.staniak@gmail.com>
2024-11-03 02:17:00 +0100

    inserted a non-numeric string

--- index.js
+++ index.js
@@ -272,5 +272,5 @@
 
     console.log(sum(10, 20, 30));
     console.log(sum(10, "20", 30)); // problem removed by Number casting
-    console.log(sum(10, "20", "030"));
+    console.log(sum(10, "20", "abc", "030"));
 }

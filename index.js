77732b59e1ad5fc36194cde82b882c198a47ef6e
bs <bartosz.staniak@gmail.com>
2024-11-04 21:45:08 +0100

    inserted an empty line

--- index.js
+++ index.js
@@ -323,6 +323,7 @@
 {
     function ignoreFirst(...[, b, c]) {
         console.log("The first arguments element: " + arguments[0]);
+        
         return (b ?? 0) + (c ?? 0);
     }
     ignoreFirst();

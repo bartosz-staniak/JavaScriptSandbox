f5dcabe58e6b8bec358629e01a99bb819dbb3d05
bs <bartosz.staniak@gmail.com>
2024-11-10 16:25:08 +0100

    updated a comment

--- index.js
+++ index.js
@@ -364,7 +364,7 @@
         return sortedArgs;
     }
 
-    console.log(sortRestArgs(1, 3, 2, 7, 12 , 0)); // [ 0, 1, 12, 2, 3, 7 ]
+    console.log(sortRestArgs(1, 3, 2, 7, 12 , 0)); // [ 0, 1, 12, 2, 3, 7 ] revisit
 }
 
 {

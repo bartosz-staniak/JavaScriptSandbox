2f491a2b4e68d40c8de08c23d7004fea6916e8a0
bs <bartosz.staniak@gmail.com>
2024-11-12 10:54:20 +0100

    removed the round brackets from the element variable

--- index.js
+++ index.js
@@ -431,7 +431,7 @@
 
 {
     function multiplyEachElementByFirstParam(multiplier, ...theElements) {
-        return theElements.map((element) => multiplier * element);
+        return theElements.map(element => multiplier * element);
     }
 
     const returnedArray = multiplyEachElementByFirstParam(-1, 0, 1, 2, 3, 4, 5, 6, 7);

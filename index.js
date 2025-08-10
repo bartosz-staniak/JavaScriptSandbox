ac1aabc92cb9bfb49eeb7d8b9c1534572e3bc7d5
bs <bartosz.staniak@gmail.com>
2025-08-10 20:43:00 +0200

    added a comment with the resulting console output

--- index.js
+++ index.js
@@ -619,7 +619,7 @@
         lastName: "Doe"
     }
 
-    console.log(person.fullName.call(person1, "Oslo", "Norway"));
+    console.log(person.fullName.call(person1, "Oslo", "Norway")); // John Doe, Oslo, Norway
 }
 
 // npx nodemon index.js
